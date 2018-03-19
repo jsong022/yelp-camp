5e9c1095099b83f72b6ab3eb65a931392b8b0dc1
jsong022 <jsong022@ucr.edu>
2018-03-19 23:18:08 +0000

    Added a .gitignore & moved the campgroundSchema definition into models/campground.js

--- app.js
+++ app.js
@@ -9,39 +9,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 //set up mongoose stuff
 mongoose.connect("mongodb://localhost/yelp-camp", {useMongoClient: true});
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
 
-/*
-//add a sample to the database
-Campground.create(
-  {name: "Salmon Creek",
-  image: "https://farm9.staticflickr.com/8491/8296644156_c60886ce08.jpg",
-  description: "This is a beautiful campsite by a river. Perfect for a family summer vacation."},
-  function(err, obj){
-    if (err){
-      console.log("Error while adding a new campground");
-      console.log(err);
-    } else{
-      console.log("Adding a new camp to the databse:");
-      console.log(obj);}});
-//sample of camps following the campgroundSchema
-  {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8491/8296644156_c60886ce08.jpg"}
-  {name: "Granite Hill", image: "https://farm9.staticflickr.com/8206/8265812638_8100a96382.jpg"}
-  {name: "Stardew Plains", image: "https://farm3.staticflickr.com/2888/9613885906_e271d89326.jpg"}
-  {name: "Sandstone Lake", image: "https://farm9.staticflickr.com/8020/7538732802_49a42d28d2.jpg"}
-  {name: "Bear Mountain", image: "https://farm8.staticflickr.com/7448/9662927256_9fcc19b9e8.jpg"}
-  {name: "Canyon Peak", image: "https://farm6.staticflickr.com/5255/5571147697_295d6327e1.jpg"}
-  {name: "Eagle Nest Park", image: "https://farm8.staticflickr.com/7516/28211128032_538751eefa.jpg"}
-  {name: "Camp Riverland", image: "https://farm4.staticflickr.com/3831/12497870654_a04bf51e08.jpg"}
-*/
+//require models
+var Campground = require("./models/campground.js");
 
 //root route
 app.get("/", function(req, res){
